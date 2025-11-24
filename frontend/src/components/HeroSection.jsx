740a3582e76b98a83c2f94e5821622c89959e2b1
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-11-24 23:21:32 +0530

    refactor: remove unused code from HeroSection and clean up HomePage component

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,40 +1,9 @@
 import Navbar from "./navbar";
-import interview from "../assets/interview.png";
 
 const HeroSection = () => {
   return (
     <>
-      <Navbar />
-   <main className="overflow-x-hidden">
-        {/* HERO SECTION */}
-        <section>
-          <div className="pb-24 pt-12 md:pb-32 lg:pb-56 lg:pt-44">
-            <div className="relative mx-auto flex max-w-6xl flex-col px-6 lg:block">
-              {/* TEXT */}
-              <div className="mx-auto max-w-lg text-center lg:ml-0 lg:w-1/2 lg:text-left">
-                <h1 className="mt-8 max-w-2xl text-5xl font-medium md:text-6xl xl:text-7xl">
-                  Ship 10x Faster with NS
-                </h1>
-
-                <p className="mt-8 max-w-2xl text-lg text-gray-600">
-                  Highly customizable components for building modern websites
-                  and applications that look and feel the way you mean it.
-                </p>
-
-                {/* IMAGE */}
-              <img
-                className="right-0 top-0 mt-5 rounded-2xl w-full lg:mt-0 lg:absolute lg:w-1/2"
-                src={interview}
-                alt="Abstract Object"
-              />
-
-              </div>
-
-              
-            </div>
-          </div>
-        </section>
-      </main>
+      
     </>
   );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import toast from 'react-hot-toast'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 import HeroSection from '../components/HeroSection.jsx'
+import Navbar from '../components/navbar.jsx'
 
 const HomePage = () => {
   return (
     <>
+      <Navbar />
       <HeroSection />
         <button className='btn btn-primary mt-20' onClick={() => toast.success('Signed in successfully!')}>click me</button>
 
